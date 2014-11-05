95dc4a9cb7bff5f021958e0d45b2c28ad6da301f
Ryan Paul <seg@phault.net>
2014-11-05 11:13:08 -0800

    Limit map view to 100 markers at a time

--- public/script/app.js
+++ public/script/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
   map.addLayer(L.tileLayer(mapTiles, {attribution: mapAttrib}));
 
   var template = Handlebars.compile($("#cat-template").html());
+  var markers = [];
 
   $("#map").hide();
 
@@ -24,16 +25,19 @@ $(document).ready(function() {
     $("#cats").prepend(template(cat));
 
     if (cat.place) {
-      var marker = L.marker(L.latLng(
+      var count = markers.unshift(L.marker(L.latLng(
           cat.place.coordinates[1],
-          cat.place.coordinates[0]));
+          cat.place.coordinates[0])));
 
-      map.addLayer(marker);
-      marker.bindPopup(
+      map.addLayer(markers[0]);
+      markers[0].bindPopup(
           "<img src=\"" + cat.images.thumbnail.url + "\">",
           {minWidth: 150, minHeight: 150});
       
-      marker.openPopup();
+      markers[0].openPopup();
+
+      if (count > 100)
+        map.removeLayer(markers.pop());
     }
   }
 
@@ -41,7 +45,7 @@ $(document).ready(function() {
   
   socket.on("cat", addCat); 
   socket.on("recent", function(data) {
-    data.forEach(addCat);
+    data.reverse().forEach(addCat);
   });
 
 });
